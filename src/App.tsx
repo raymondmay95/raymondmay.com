957f3e5e70bf9a2bbb6b780be3df8e665241c1b5
raymondmay95 <raymond.may.95@outlook.com>
2023-08-17 21:35:33 -1000

    pushing to prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./Routes";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 1)
+  })
   return (
     <BrowserRouter>
       <Routes />
