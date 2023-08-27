3b3c8ea08ebe119830eec95a0f303a10d34ab469
raymondmay95 <raymond.may.95@outlook.com>
2023-08-26 15:22:58 -1000

    made base orange theme

--- src/pages/authorized/AuthorizedLayout.tsx
+++ src/pages/authorized/AuthorizedLayout.tsx
@@ -1,9 +1,11 @@
-import { Logout, Menu as MenuIcon, Person, Settings } from '@mui/icons-material';
-import { Avatar, Drawer, IconButton, Menu, MenuItem, MenuList, MenuProps, Stack, Toolbar, alpha, styled } from "@mui/material";
-import { deepOrange } from '@mui/material/colors';
+import { CalendarTodayOutlined, HomeOutlined, Logout, Menu as MenuIcon, NoteAltOutlined, Person, Settings, TaskOutlined } from '@mui/icons-material';
+import { Avatar, Container, Divider, Drawer, IconButton, Menu, MenuItem, MenuItemProps, MenuList, MenuProps, Stack, Toolbar, alpha, styled } from "@mui/material";
+import { deepOrange, orange } from '@mui/material/colors';
 import { useState } from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, NavLinkProps, Outlet, useLocation } from 'react-router-dom';
 
+const themeColor = orange
+const drawerWidth = 150
 
 export default function AuthorizedLayout() {
     const { pathname } = useLocation()
@@ -23,14 +25,27 @@ export default function AuthorizedLayout() {
         setOpenDrawer(false)
     }
     return (
-        <Stack>
+        <>
             <Drawer
                 anchor='left'
                 variant='temporary'
                 open={openDrawer}
                 onClose={handleCloseDrawer}
+                PaperProps={{
+                    sx: {
+                        bgcolor: themeColor[50],
+                        width: drawerWidth,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }
+                }}
+                sx={(theme) => ({
+                    bgcolor: alpha(themeColor[800], theme.palette.action.selectedOpacity) + ' !important'
+                }
+                )}
             >
-                <Toolbar disableGutters sx={{ pl: 2 }}>
+                <Toolbar disableGutters>
                     <img
                         loading="eager"
                         src='/assets/Raymond-Arthur_May.svg'
@@ -39,79 +54,64 @@ export default function AuthorizedLayout() {
                         height={75}
                     />
                 </Toolbar>
+                <Divider flexItem light />
                 <MenuList
+                    disablePadding
                     sx={{
-                        width: 200,
-                        height: 55
+                        backgroundColor: themeColor[50],
                     }}
                 >
-                    <MenuItem
+                    <StyledNavMenuItem
                         selected={pathname === '/authorized/home'}
                         to='/authorized/home'
                         component={NavLink}
+                        onClick={handleCloseDrawer}
                     >
+                        <HomeOutlined />
                         Home
-                    </MenuItem>
-                    <MenuItem
+                    </StyledNavMenuItem>
+                    <StyledNavMenuItem
                         selected={pathname === '/authorized/schedule'}
                         to='/authorized/schedule'
                         component={NavLink}
+                        onClick={handleCloseDrawer}
                     >
+                        <CalendarTodayOutlined />
                         Schedule
-                    </MenuItem>
-                    <MenuItem
+                    </StyledNavMenuItem>
+                    <StyledNavMenuItem
                         selected={pathname === '/authorized/notes'}
                         to='/authorized/notes'
                         component={NavLink}
+                        onClick={handleCloseDrawer}
                     >
+                        <NoteAltOutlined />
                         Notes
-                    </MenuItem>
-                    <MenuItem
+                    </StyledNavMenuItem>
+                    <StyledNavMenuItem
                         selected={pathname === '/authorized/tasks'}
                         to='/authorized/tasks'
                         component={NavLink}
+                        onClick={handleCloseDrawer}
                     >
+                        <TaskOutlined />
                         Tasks
-                    </MenuItem>
+                    </StyledNavMenuItem>
                 </MenuList>
             </Drawer>
-
-            <Toolbar
-                sx={{
-                    backgroundColor: 'rgba(255,255,255,.8)',
-                    justifyContent: 'space-between'
-                }}>
-                <IconButton onClick={handleOpenDrawer}>
-                    <MenuIcon />
-                </IconButton>
-                <IconButton
-                    onClick={handleClick}
-                    id='avatar-button'
-                    sx={{
-                        width: 45,
-                        height: 45
-                    }}
-                >
-                    <Avatar
-                        sx={{
-                            bgcolor: deepOrange[400],
-                            fontSize: 16,
-                        }}
-                    >
-                        RM
-                    </Avatar>
-                </IconButton>
-            </Toolbar>
-
             <ProfileMenu
+                elevation={1}
                 open={open}
                 anchorEl={anchorEl}
                 onClose={handleClose}
                 MenuListProps={{
                     'aria-labelledby': 'avatar-button',
+                    sx: {
+                        backgroundColor: themeColor[50],
+                    }
                 }}
             >
-                <Stack spacing={1} my={1}>
+                <Stack spacing={.5} p={.75}>
                     <MenuItem component={NavLink} to='/profile'>
                         <Person />
                         Profile
@@ -120,20 +120,90 @@ export default function AuthorizedLayout() {
                         <Settings />
                         Settings
                     </MenuItem>
+                    <Divider />
                     <MenuItem component={NavLink} to='/logout'>
                         <Logout />
                         Log Out
                     </MenuItem>
                 </Stack>
             </ProfileMenu>
-            <Outlet />
-        </Stack>
+            <Stack spacing={3}>
+                <Toolbar
+                    sx={{
+                        backgroundColor: alpha(themeColor[50], .2),
+                        justifyContent: 'space-between'
+                    }}>
+                    <IconButton onClick={handleOpenDrawer}>
+                        <MenuIcon />
+                    </IconButton>
+                    <IconButton
+                        onClick={handleClick}
+                        id='avatar-button'
+                        sx={{
+                            width: 45,
+                            height: 45
+                        }}
+                    >
+                        <Avatar
+                            sx={{
+                                bgcolor: deepOrange[400],
+                                fontSize: 16,
+                            }}
+                        >
+                            RM
+                        </Avatar>
+                    </IconButton>
+                </Toolbar>
+                <Container sx={{ margin: 4 }}>
+                    <Outlet />
+                </Container>
+            </Stack>
+        </>
     )
 }
+
+const StyledNavMenuItem = styled((props: MenuItemProps & NavLinkProps) => (
+    <MenuItem {...props} />
+))(({ theme, selected }) => ({
+    height: 55,
+    fontSize: 18,
+    width: drawerWidth,
+    backgroundColor: selected
+        ? alpha(
+            themeColor[800],
+            theme.palette.action.selectedOpacity,
+        ) + ' !important'
+        : '',
+    color: selected ? theme.palette.text.primary : theme.palette.text.secondary,
+    '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: selected ? theme.palette.text.primary : theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+    },
+    '&$selected': {
+        backgroundColor: alpha(
+            themeColor[800],
+            theme.palette.action.selectedOpacity,
+        ) + ' !important',
+    },
+    '&:active': {
+        backgroundColor: alpha(
+            themeColor[800],
+            theme.palette.action.selectedOpacity,
+        ),
+    },
+    '&:hover': {
+        backgroundColor: alpha(
+            themeColor[800],
+            theme.palette.action.selectedOpacity,
+        ),
+    },
+}))
 
 
 const ProfileMenu = styled((props: MenuProps) => (
     <Menu
+        title='Profile Menu'
         elevation={0}
         anchorOrigin={{
             vertical: 'bottom',
@@ -147,15 +217,19 @@ const ProfileMenu = styled((props: MenuProps) => (
     />
 ))(({ theme }) => ({
     '& .MuiPaper-root': {
-        borderRadius: 6,
-        marginTop: theme.spacing(1),
-        minWidth: 140,
+        borderRadius: 8,
+        marginTop: theme.spacing(.5),
         color:
             theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
         boxShadow:
-            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+            ` 
+              ${alpha(themeColor[400], theme.palette.action.selectedOpacity)} 0px 1px 0px 1px,
+              ${alpha(themeColor[800], theme.palette.action.selectedOpacity)} 0px 0px 0px 1px,
+              ${alpha(themeColor[400], theme.palette.action.selectedOpacity)} 0px 10px 15px -3px,
+              ${alpha(themeColor[800], theme.palette.action.selectedOpacity)} 0px 4px 6px -2px
+            `,
         '& .MuiMenu-list': {
-            padding: '4px 0',
+            padding: '0',
         },
         '& .MuiMenuItem-root': {
             '& .MuiSvgIcon-root': {
@@ -165,7 +239,13 @@ const ProfileMenu = styled((props: MenuProps) => (
             },
             '&:active': {
                 backgroundColor: alpha(
-                    theme.palette.primary.main,
+                    themeColor[800],
+                    theme.palette.action.selectedOpacity,
+                ),
+            },
+            '&:hover': {
+                backgroundColor: alpha(
+                    themeColor[800],
                     theme.palette.action.selectedOpacity,
                 ),
             },
